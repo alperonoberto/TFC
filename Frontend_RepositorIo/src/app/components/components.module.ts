e4e046dec5b114cdfd0514f9aa6c70b828bcc325
alperonoberto <alperonoberto@gmail.com>
2023-05-07 19:17:07 +0200

    feat: login page

--- Frontend_RepositorIo/src/app/components/components.module.ts
+++ Frontend_RepositorIo/src/app/components/components.module.ts
@@ -6,13 +6,16 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { HomeComponent } from './home/home.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,9 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    RouterModule
+    RouterModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   exports: [
     ToolbarComponent,
--- Frontend_RepositorIo/src/app/components/login/login.component.ts
+++ Frontend_RepositorIo/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  submit(){}
 }

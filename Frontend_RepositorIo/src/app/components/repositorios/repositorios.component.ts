4fa96688df86963e2bd4186bf0fed5f399f418c1
alperonoberto <alperonoberto@gmail.com>
2023-05-21 18:03:44 +0200

    feat: repositorios component

--- Frontend_RepositorIo/src/app/components/repositorios/repositorios.component.ts
+++ Frontend_RepositorIo/src/app/components/repositorios/repositorios.component.ts
@@ -7,4 +7,76 @@ import { Component } from '@angular/core';
 })
 export class RepositoriosComponent {
 
+  public repositorioActual: number = 0;
+
+  public listaRepositorios = [
+    {
+      id: 1,
+      nombre: "Repositorio1",
+      descripcion: "Descripcion repositorio 1",
+      fechaMod: null,
+      cuentaId: 1
+    },
+    {
+      id: 2,
+      nombre: "Repositorio2",
+      descripcion: "Descripcion repositorio 2",
+      fechaMod: null,
+      cuentaId: 1
+    },
+    {
+      id: 3,
+      nombre: "Repositorio3",
+      descripcion: "Descripcion repositorio 3",
+      fechaMod: null,
+      cuentaId: 1
+    },
+    {
+      id: 4,
+      nombre: "Repositorio4",
+      descripcion: "Descripcion repositorio 4",
+      fechaMod: null,
+      cuentaId: 1
+    },
+    {
+      id: 5,
+      nombre: "Repositorio5",
+      descripcion: "Descripcion repositorio 5",
+      fechaMod: null,
+      cuentaId: 1
+    }
+  ];
+
+  public listaArchivos = [
+    {
+      nombre: "archivo1.jpg",
+      fechaMod: null,
+      repositorioId: 1
+    },
+    {
+      nombre: "archivo2.pdf",
+      fechaMod: null,
+      repositorioId: 1
+    },
+    {
+      nombre: "archivo3.html",
+      fechaMod: null,
+      repositorioId: 1
+    },
+    {
+      nombre: "archivo4.md",
+      fechaMod: null,
+      repositorioId: 1
+    },
+    {
+      nombre: "archivo5.scss",
+      fechaMod: null,
+      repositorioId: 1
+    },
+  ];
+
+  mostrarArchivos(repositorioId: number) {
+    this.repositorioActual = repositorioId;
+  }
+
 }
